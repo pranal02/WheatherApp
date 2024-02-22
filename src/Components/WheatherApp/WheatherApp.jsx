f5e4b5f8562bbe2544c351c9b40d5fdfc9dcd75d
Pranal Wankhade <pranalwankhade002@gmail.com>
2024-02-23 01:38:43 +0530

    container styled

--- src/Components/WheatherApp/WheatherApp.jsx
+++ src/Components/WheatherApp/WheatherApp.jsx
@@ -11,7 +11,14 @@ import humidity_icon from '../Assets/humidity.png'
 
 const WeatherApp = () => {
   return (
-    <div>
+    <div className='container'>
+        <div className='top-bar'>
+            <input type="text" className="cityInput"  placeholder='Search'/>
+            <div className='search-icon'>
+                <img src={search_icon} alt=''/>
+            </div>
+
+        </div>
 
     </div>
   )
